e799d31e218d99fd4f0c50833aa18cb806d147cd
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-14 12:45:53 +0530

    done

---
+++ src/Evenodd.java
@@ -0,0 +1,14 @@
+public class Evenodd {
+
+    public static void main(String[] args) {
+        Integer a=20;
+        if(a%2==0){
+            System.out.println("even");
+
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
---
+++ src/Greaterless.java
@@ -0,0 +1,15 @@
+public class Greaterless {
+
+    public static void main(String[] args) {
+        Integer a=10;
+        if(a<20)
+        {
+            System.out.println("less");
+
+        }
+        else
+        {
+            System.out.println("greater");
+        }
+    }
+}
