ac1e7cfcd224c0646c1f4e62cfe7e535f18135b8
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-14 12:36:01 +0530

    done

--- src/hello.java
+++ src/hello.java
@@ -1,21 +1,12 @@
 public class hello {
     public static void main(String[] args) {
 
-
-        Integer a=21;
-        if(a<20)
-        {
-            System.out.println("less than 20");
-
-        }
-        else
-        {
-            System.out.println("greater than 20");
-        }
-
-
-
-
-
+        Integer a=10;
+        a++;
+        System.out.println(a);
     }
+
 }
+
+
+
